f5b2297296edc0de68d9fc15cdad5e65b1623fd7
ThaSMorato <tha.s.morato@gmail.com>
2023-11-21 18:40:06 -0300

    transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
+
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { PriceHighLight, TransactionsTable, TranscationsContainer } from './styles';
 
 export const Transactions = () => {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TranscationsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hanburguer</td>
+              <td>
+                <PriceHighLight variant="outcome"> - R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentacao</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TranscationsContainer>
     </div>
   );
 };
+
